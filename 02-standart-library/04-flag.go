faea3d809bac954f45928da336395a6ce3ea9a94
Siintya <ajeng.sintya15@gmail.com>
2026-05-06 18:08:03 +0700

    Update 04-flag.go

---
+++ 02-standart-library/04-flag.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+/**Package flag 
+berisi fungsionalitas untuk memparsing command line argument
+
+**/
+
+func main() {
+	var username = flag.String("username", "root", "database username")
+	var password = flag.String("password", "root", "database password")
+	var host = flag.String("host", "localhost", "database host")
+	var port = flag.Int("port", 0, "database port")
+
+	flag.Parse()
+
+	fmt.Println("Username", *username)
+	fmt.Println("Password", *password)
+	fmt.Println("Host", *host)
+	fmt.Println("Port", *port)
+
+	/**Jika ingin mengganti  masukan parameter berikut ini, ketika 
+	run file golang:
+	$ go run 04-flag.go -username=sintya -password=rahasia -host=123.123.123.3 -port=5505
+	**/
+
+}
+
+
+
