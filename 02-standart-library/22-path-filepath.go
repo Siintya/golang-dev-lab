2168d144ec1014dc2d3bada6c4454d3498f1b9e7
Siintya <ajeng.sintya15@gmail.com>
2026-05-07 18:40:30 +0700

    Upload 22-path-file-path.go

---
+++ 02-standart-library/22-path-filepath.go
@@ -0,0 +1,33 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"path/filepath"
+)
+
+/**Package Path/filepath
+* digunakan untuk memanipulasi data path seperti path di URL
+  atau path di File
+* scr default, package ini menggunakan slash (/) sbg karakter path-nya.
+  Oleh karna itu, cocok untuk data URL
+* Namun, jika ingin menggunakan untuk memanipulasi di File System, karna
+  OS Windows menggunakan backslash (\), maka khusus untuk File System tsb
+  perlu menggunakan package path/filepath
+https://pkg.go.dev/path
+https://pkg.go.dev/path/filepath
+**/
+
+func main() {
+	// Directory atau Folder
+	fmt.Println(filepath.Dir("hello/world.go")) //hello
+	// File
+	fmt.Println(filepath.Base("hello/world.go")) // world.go
+	// Ext -> Extention
+	fmt.Println(filepath.Ext("hello/world.go"))             // .go
+	fmt.Println(filepath.Join("hello", "world", "main.go")) //hello/world/main.go
+	// IsAbs -> Is Absolut
+	fmt.Println(filepath.IsAbs("hello/world.go"))
+	fmt.Println(filepath.IsLocal("hello/world.go"))
+}
