e7657288261d5f84ddf0b2ce27e4d6daad30dbe5
Siintya <ajeng.sintya15@gmail.com>
2026-05-07 14:29:29 +0700

    Update

--- 02-standart-library/13-reflect.go
+++ 02-standart-library/13-reflect.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"reflect"
 )
 
 /**Package reflect (reflection)
-* dlm bahasa pemrograman, biasanya ada fitur Reflection, dimana 
+* dlm bahasa pemrograman, biasanya ada fitur Reflection, dimana
   kita bisa melihat struktur kode kita pada saat aplikasi sedang berjalan
 * hal ini bisa dilakukan di golang dg menggunakan package reflect
-* Reflection sangat berguna ketika kita ingin membuat library 
+* Reflection sangat berguna ketika kita ingin membuat library
   yg general sehingga mudah digunakan
   https://pkg.go.dev/reflect
 **/
@@ -17,7 +17,14 @@ type Sample struct {
 	Name string
 }
 
-func main() {
+type Person struct {
+	Name string
+}
 
+func readFile(value array) {
+	var valueType reflect.Value = reflect.TypeOf(value)
+}
+
+func main() {
 
 }
