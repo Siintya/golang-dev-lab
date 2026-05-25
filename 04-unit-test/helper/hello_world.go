0709b3447be656924793bdca154dc8f7e4770599
Siintya <ajeng.sintya15@gmail.com>
2026-05-25 18:55:36 +0700

    Update

--- 04-unit-test/helper/hello_world.go
+++ 04-unit-test/helper/hello_world.go
@@ -1,5 +1,5 @@
 package helper
 
 func HelloWorld(name string) string {
-	return "HI " + name
+	return "Hello " + name
 }
---
+++ 04-unit-test/helper/subtest_hello_world_test.go
@@ -0,0 +1,25 @@
+package helper
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+/*
+	Sub Test
+
+Golang mendukung fitur pembuatan function unit test didalam
+function unit test dinamakan sub test.
+*/
+func TestSubTest(t *testing.T) {
+	t.Run("Sintya", func(t *testing.T) {
+		result := HelloWorld("Sintya")
+		require.Equal(t, "Hello Sintya", result, "Result must be 'Hello Sintya'")
+	})
+
+	t.Run("Lestari", func(t *testing.T) {
+		result := HelloWorld("Lestari")
+		require.Equal(t, "Hello Lestari", result)
+	})
+}
