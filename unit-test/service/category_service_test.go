45bf4235e9ad101ef5032120261f5a5a5a6ce38f
Siintya <ajeng.sintya15@gmail.com>
2026-05-27 02:38:47 +0700

    update

--- unit-test/service/category_service_test.go
+++ unit-test/service/category_service_test.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"testing"
+	"unit-test/entity"
 	"unit-test/repository"
 
 	"github.com/stretchr/testify/assert"
@@ -10,7 +11,7 @@ import (
 
 var categoryRepository = repository.CategoryRepositoryMock{Mock: mock.Mock{}}
 
-func TestCategoryService_Get(t *testing.T) {
+func TestCategoryService_GetFound(t *testing.T) {
 	// program mock
 	categoryRepository.Mock.On("FindById", "1").Return(nil)
 
@@ -22,4 +23,25 @@ func TestCategoryService_Get(t *testing.T) {
 
 	assert.Nil(t, category)
 	assert.NotNil(t, err)
+}
+
+func TestCategoryService_GetSuccess(t *testing.T) {
+	category := entity.Category{
+		Id:   "1",
+		Name: "Laptop",
+	}
+
+	// program mock
+	categoryRepository.Mock.On("FindById", "2").Return(category)
+
+	categoryService := CategoryService{
+		Repository: &categoryRepository,
+	}
+
+	result, err := categoryService.Get("2")
+
+	assert.Nil(t, err)
+	assert.NotNil(t, result)
+	assert.Equal(t, category.Id, result.Id)
+	assert.Equal(t, category.Name, result.Name)
 }
