99c8ad01abf168935d96878d334808b06a0d397f
Siintya <ajeng.sintya15@gmail.com>
2026-07-20 14:11:23 +0700

    updated

--- 01-fundamentals/20-slice.go
+++ 01-fundamentals/20-slice.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	/**Slice 
-	 # adalah tipe data yang merepresentasikan potongan dari array. 
-	 # Berbeda dengan array, slice tidak memiliki panjang tetap sehingga ukurannya dapat berubah (dinamis).	
+	/**Slice
+	 # adalah tipe data yang merepresentasikan potongan dari array.
+	 # Berbeda dengan array, slice tidak memiliki panjang tetap sehingga ukurannya dapat berubah (dinamis).
 	**/
 	fruits := [...]string{
 		"Banana",
@@ -33,4 +33,33 @@ func main() {
 	// 4. array[:] -> membuat slice dari array dimulai index 0 sampai index akhir di array
 	slice4 := fruits[:]
 	fmt.Println(slice4)
+
+	// make() -> membuat slice baru dengan panjang dan kapasitas tertentu
+	score := []int{5, 10, 20}
+	fmt.Println("Skor saat ini", score)
+
+	score2 := make([]int, 3) // membuat slice baru dengan panjang 3 dan kapasitas 3
+	fmt.Println("Skor saat ini", score2)
+
+	// 1. Membuat slice lewat "Slice Literal"
+	buah := []string{"Apel", "Jeruk", "Mangga"}
+	fmt.Printf("Awal  -> Len: %d, Cap: %d, Data: %v\n", len(buah), cap(buah), buah)
+	// Output: Len: 3, Cap: 3, Data: [Apel Jeruk Mangga]
+
+	// 2. Terjadi "Growth" (Pertumbuhan) karena kapasitas awal (3) sudah penuh
+	// Go secara otomatis mencarikan tempat memori baru yang kapasitasnya lebih besar
+	buah = append(buah, "Pisang")
+	fmt.Printf("Baru  -> Len: %d, Cap: %d, Data: %v\n", len(buah), cap(buah), buah)
+	// Output: Len: 4, Cap: 6, Data: [Apel Jeruk Mangga Pisang]
+
+	sliceData := []string{"Go", "Java", "Python"}
+
+	// 1. Konversi langsung ke Array (Fitur Go 1.20+)
+	// Ukuran array harus sama dengan atau lebih kecil dari panjang slice
+	arr := [3]string(sliceData)
+	fmt.Println(arr) // Output: [Go Java Python]
+
+	// 2. Konversi ke Pointer Array
+	arrPtr := (*[3]string)(sliceData)
+	fmt.Println(*arrPtr) // Output: [Go Java Python]
 }
